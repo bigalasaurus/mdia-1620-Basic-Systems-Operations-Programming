59f63ed2c9b1242ce561a669b7e552cac7dc5eb4
bigalasaurus <14AlexDuncan@gmail.com>
2022-09-16 12:22:43 -0700

    adding code

---
+++ part1/week2/test.js
@@ -0,0 +1,16 @@
+// Data Type - Object
+/**
+ * 6-4-1 Create a variable called grannySmith and add the
+ * following values (use camelCase):
+ * colour of the apple is green
+ * Origin location is Australia
+ * origin date is 1868
+ * then console log out the colour from the object
+ */
+ var grannySmith = {
+    color: "green",
+    originLocation: "Australia",
+    originDate: 1868
+    };
+    
+    console.log(grannySmith);
